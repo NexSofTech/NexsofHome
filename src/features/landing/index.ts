e50ff5c613625ebc4afaaee9b6b1017f7f7a0b1f
barryspacezero <barryspace01@gmail.com>
2026-04-17 01:50:38 +0530

    Restructured the codebase to a much simpler architecture which is easy to understand

---
+++ src/features/landing/index.ts
@@ -0,0 +1,6 @@
+export { Hero } from './components/Hero';
+export { HeroContent } from './components/HeroContent';
+export { TrustedBy } from './components/TrustedBy';
+export { EndToEnd } from './components/EndToEnd';
+export { Services } from './components/Services';
+export { Team } from './components/Team';
