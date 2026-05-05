a30b19b9725f11d1d2024b57218de8e919c0c5a4
Vinay <nexsof01tech@gmail.com>
2026-05-05 15:45:03 +0530

    Gtag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Outfit, Raleway } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const outfit = Outfit({
@@ -20,7 +21,26 @@ export default function RootLayout({
 }) {
   return (
     <html className={`${outfit.variable} ${raleway.variable}`}>
-      <body className="font-sans">{children}</body>
+      <body className="font-sans">
+        {/* Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-W5MD6QR81L`}
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-W5MD6QR81L');
+            `,
+          }}
+        />
+        {children}
+      </body>
     </html>
   );
 }
