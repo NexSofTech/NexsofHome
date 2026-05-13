842fda0745925b7668c0f433a3c2394419517b34
NexSofTech <nexsoftech01@gmail.com>
2026-05-13 16:54:41 +0530

    Update page.tsx

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -14,9 +14,9 @@ import { Footer } from "@/components/layout/Footer";
 
 export default function HomePage() {
   return (
-    <main className="bg-white min-h-screen pt-4 md:pt-6">
+    <main className="bg-black md:bg-white min-h-screen md:pt-6">
       {/* Framed top section containing Navbar and Hero */}
-      <div className="mx-4 md:mx-6 lg:mx-8 rounded-[40px] md:rounded-[50px] overflow-hidden bg-black relative flex flex-col min-h-[calc(100vh-2rem)] md:min-h-[calc(100vh-3rem)] border border-white/10">
+      <div className="mx-0 md:mx-6 lg:mx-8 rounded-none md:rounded-[50px] overflow-hidden bg-black relative flex flex-col min-h-screen md:min-h-[calc(100vh-3rem)] border-0 md:border md:border-white/10">
         <Navbar />
         {/* Separator bar at the bottom of the nav bar */}
         <div className="h-px bg-white/60 w-[96%] mx-auto relative z-20" />
